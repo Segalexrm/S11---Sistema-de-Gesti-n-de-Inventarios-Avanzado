b9b58d2871c71157db388ac6829597ad08ac46cc
Segalexrm <alexrosado1996@hotmail.com>
2026-03-01 17:59:32 -0500

    Añadido el método to_dict,servirá para la persistencia de datos.

--- modelos/producto.py
+++ modelos/producto.py
@@ -1,42 +1,36 @@
-# modelos/producto.py
-
 class Producto:
     def __init__(self, id_producto, nombre, cantidad, precio):
-        self.__id = id_producto  # Atributo privado
+        self.__id = id_producto
         self.__nombre = nombre
         self.__cantidad = cantidad
         self.__precio = precio
 
-    # Getters y Setters (Encapsulamiento)
+    # Getters
     @property
-    def id(self):
-        return self.__id
+    def id(self): return self.__id
 
     @property
-    def nombre(self):
-        return self.__nombre
+    def nombre(self): return self.__nombre
 
+    @property
+    def cantidad(self): return self.__cantidad
+
+    @property
+    def precio(self): return self.__precio
+
+    # Setters
     @nombre.setter
-    def nombre(self, valor):
-        self.__nombre = valor
-
-    @property
-    def cantidad(self):
-        return self.__cantidad
+    def nombre(self, valor): self.__nombre = valor
 
     @cantidad.setter
-    def cantidad(self, valor):
-        if valor >= 0:
-            self.__cantidad = valor
-
-    @property
-    def precio(self):
-        return self.__precio
+    def cantidad(self, valor): self.__cantidad = valor
 
     @precio.setter
-    def precio(self, valor):
-        if valor >= 0:
-            self.__precio = valor
+    def precio(self, valor): self.__precio = valor
 
     def __str__(self):
-        return f"ID: {self.__id} | Nombre: {self.__nombre} | Cantidad: {self.__cantidad} | Precio: ${self.__precio:.2f}"
+        return f"ID: {self.__id} | Nombre: {self.__nombre} | Stock: {self.__cantidad} | Precio: ${self.__precio:.2f}"
+
+    def a_diccionario(self):
+        """Facilita la serialización para el archivo."""
+        return f"{self.__id},{self.__nombre},{self.__cantidad},{self.__precio}"
